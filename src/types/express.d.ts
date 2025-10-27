2dd65cac237812e2dbf253d925ff604004d4b8e2
Aman-jha12 <ajha82703@gmail.com>
2025-10-27 23:02:55 +0530

    Modified types folder

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,14 +1,16 @@
-// This tells TypeScript that req can have a user property.
-import { Role } from '@prisma/client';
+import { Role } from '@Prisma/client';
 
+// This 'declare global' is the magic part.
+// It merges this definition with the original 'express' definitions.
 declare global {
   namespace Express {
     export interface Request {
+      // This tells TypeScript that the 'user' property can exist
+      // on the Request object.
       user?: {
         id: string;
         role: Role;
       };
-      file?: any; // For Multer
     }
   }
 }
